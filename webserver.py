81c979dcd63748e28f3eab63c7a4541be67576cb
Michael Wenk <mikewenk@gmail.com>
2020-10-20 04:53:07 -0700

    Changed hello world to something else

--- webserver.py
+++ webserver.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def return_heartbeat():
     try:
         data = readHeartbeat()
     except Exception as err: 
